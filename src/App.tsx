b2f037353ef70fc44e3ba4fbeb9ff4db1a3b8516
Moon Hyo Chan <gycksl14@naver.com>
2026-03-14 04:03:42 +0900

    fix(components): 루트 경로에서 로그인 페이지 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '@pages/Login'
-import { COLORS, TEXT_STYLES } from '@/constants'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className={`min-h-screen ${COLORS.background.main} ${TEXT_STYLES.size.base}`}>
-              TEAMITZ
-            </div>
-          }
-        />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
